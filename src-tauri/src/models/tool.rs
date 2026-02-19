a2384f83178fb53e9440f98c424f9f60de424bf3
radeonvii <radeonk@foxmail.com>
2026-02-19 19:32:45 +0800

    feat: add NodeStatus model

--- src-tauri/src/models/tool.rs
+++ src-tauri/src/models/tool.rs
@@ -22,3 +22,11 @@ pub struct PlatformInfo {
     pub platform_name: String,
     pub arch: String,
 }
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct NodeStatus {
+    pub node_installed: bool,
+    pub node_version: Option<String>,
+    pub nvm_installed: bool,
+    pub nvm_version: Option<String>,
+}
